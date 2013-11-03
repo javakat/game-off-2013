452fa51de004cdfab93017c5edd92d1dd449918b
Jake <javakatdesign@gmail.com>
2013-11-02 20:48:25 -0500

    Add hero, hero.js

---
+++ unity/Assets/Scripts/hero.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var scale : double;
+
+private var thisTransform : Transform;
+private var character : CharacterController;
+private var velocity : Vector3;
+
+function Start () {
+	thisTransform = GetComponent(Transform);
+	character = GetComponent(CharacterController);
+}
+
+function Update () {
+	var movement = Vector3(Input.GetAxis('Horizontal'), Input.GetAxis('Vertical'), 0);
+	movement.Normalize();
+	
+	velocity = character.velocity;
+	movement += velocity;
+	movement *= Time.deltaTime;
+	movement *= scale;
+	
+	character.Move( movement );
+}
