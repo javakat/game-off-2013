1137454b2190f7401338c3237b88dc3fec16c577
Jake <javakatdesign@gmail.com>
2013-11-04 19:12:07 -0600

    Add spawner behavior

---
+++ unity/Assets/Scripts/SpawnerBehavior.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var rate : int; // in s
+var needsToSpawn : boolean;
+var fuzzy : Rigidbody;
+
+private var time = 0;
+
+function Start () {
+	needsToSpawn = false;
+	rate = 60;
+}
+
+function Update () {
+	if(needsToSpawn) {
+		needsToSpawn = trySpawn();
+		if(needsToSpawn) return;
+	}
+	if(++time >= rate) {
+		needsToSpawn = true;
+		time = 0;
+	}
+}
+
+function trySpawn() {
+	var newFuzz : Rigidbody = Instantiate(fuzzy, transform.position, transform.rotation);
+	return true;
+}
