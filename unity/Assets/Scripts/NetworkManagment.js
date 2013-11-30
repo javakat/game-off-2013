9c3995609484d2abd4551f4b5fbc230a80c974f4
Sean Tyler Atwell <sta0003@auburn.edu>
2013-11-29 22:35:01 -0600

    added some scripts and prefabs

---
+++ unity/Assets/Scripts/NetworkManagment.js
@@ -0,0 +1,98 @@
+﻿#pragma strict
+import System.Net;
+
+private var playerCount: int = 0;
+private var connecting = false;
+public var playerSphere: GameObject;
+
+function Awake() {
+		
+	if(Network.player.ipAddress == "172.17.9.137")
+	{
+		// Use NAT punchthrough if no public IP present
+		Network.InitializeServer(32, 25002, !Network.HavePublicAddress());
+		MasterServer.RegisterHost("NetworkTestGame", "Main Game Instance", "I luv this show");
+		
+		GameObject.Find("Number").guiText.text = "Server Instance";
+	}
+	else
+	{	
+		MasterServer.RequestHostList("NetworkTestGame");
+	}
+}
+
+function OnGUI() {
+
+	if(!connecting && !Network.isServer)
+	{	
+		var data : HostData[] = MasterServer.PollHostList();
+		// Go through all the hosts in the host list
+		
+		//Debug.Log(!connecting);
+		//Debug.Log(!Network.isServer);
+		//Debug.Log(data.Length);
+    	  	    	
+		for (var element in data)
+		{	
+			GameObject.Find("Number").guiText.text = "Client Instance";	
+			connecting = true;
+			Network.Connect(element);
+			break;
+		}
+	}
+
+	if (Network.isServer){
+		//Debug.Log("Running as a server");
+	}
+	else if (Network.isClient){
+		//Debug.Log("Running as a client");
+	}
+	
+} 
+
+//function OnPlayerConnected(player: NetworkPlayer) {	             
+//	GameObject.Find("Number").guiText.text += "     \n" + player.ipAddress + ":" + player.port;
+//}
+
+@RPC
+function LogEvent (text : String)
+{
+	GameObject.Find("Number").guiText.text += "     \nPlayer from " + text + " has connected";
+}
+
+function OnServerInitialized() {
+    Debug.Log("Server initialized and ready");
+    SpawnPlayer();
+}
+
+function OnConnectedToServer()
+{
+    Debug.Log("Successfully connected to the server");
+	networkView.RPC ("LogEvent", RPCMode.All, Network.player.ipAddress);
+    SpawnPlayer();
+}
+ 
+function OnPlayerConnected(player: NetworkPlayer) {
+    Debug.Log("Player " + " connected from " + player.ipAddress);
+}
+
+function OnPlayerDisconnected(player: NetworkPlayer) {
+	Debug.Log("Clean up after player " +  player);
+	Network.RemoveRPCs(player);
+	Network.DestroyPlayerObjects(player);
+}
+
+function SpawnPlayer()
+{
+	var x = -20;
+	var y = 17.37577;
+	var z = 151.464;
+	var posit = new Vector3(x,y,z);
+    Network.Instantiate(playerSphere, posit, Quaternion.identity, 0);
+}
+
+
+
+
+
+
---
+++ unity/Assets/Scripts/SphereController.js
@@ -0,0 +1,79 @@
+﻿#pragma strict
+
+var xscale : double;
+var yscale : double;
+var zscale : double; //likely will never NOT be 0
+
+var textA;
+var second;
+
+private var velocity : Vector3;
+
+function Start () {
+	xscale = 10.6;
+	yscale = 10.6;
+	zscale = 0;
+	
+	textA = "";
+	second = "";
+	
+	this.rigidbody.detectCollisions = true;
+}
+
+function Update () {
+	this.transform.position.z = 151.464;
+		
+	
+	velocity = Vector3(0,0,0);
+	
+    if (networkView.isMine)
+    {
+		if(Input.GetKey(KeyCode.UpArrow) == true)
+		{		
+			//this.transform.position.y += Time.deltaTime * 5;
+			velocity.y += yscale;
+		}
+		if(Input.GetKey(KeyCode.DownArrow) == true)
+		{
+			velocity.y -= yscale;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow) == true)
+		{
+			velocity.x -= xscale;
+		}
+		if(Input.GetKey(KeyCode.RightArrow) == true)
+		{
+			velocity.x += xscale;
+		}
+	}
+	
+	
+	
+	this.rigidbody.velocity = velocity;
+	
+		
+	//Debug.Log("Xscale: " + xscale);
+	//Debug.Log("Yscale: " + yscale);
+	//Debug.Log("Zscale: " + zscale);
+	//Debug.Log("Velocity velocity: " + velocity);
+	//Debug.Log("Rigidbody velocity: " + this.rigidbody.velocity);
+		
+	//GameObject.Find("watcher-base").transform.position.x += Time.deltaTime;
+
+}
+
+function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo) {
+
+	var tempVelocity : Vector3 = Vector3(0,0,0);
+	
+	if (stream.isWriting) {
+		tempVelocity = velocity;
+		stream.Serialize(tempVelocity);
+	} else {
+		stream.Serialize(tempVelocity);
+		velocity = tempVelocity;
+	}	
+	
+	
+	Debug.Log(velocity);
+}
